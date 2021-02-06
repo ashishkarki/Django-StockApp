1ea6945d0bbc139ed2662f9f3d91bb326a0bf33e
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2021-02-06 17:01:16 +0545

    adding attributes from api call to home.html

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -4,7 +4,20 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    ticker = 'aapl' # request.POST['ticker']
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token"
+                                                                                    "=pk_062031d20883444f9ea74e2610fe2011")
+    try:
+        api_response = json.loads(api_request.content)
+    except Exception as e:
+        api_response = "Error"
+
+    return render(request, 'home.html', {
+        'api_response': api_response
+    })
 
 
 def about(request):
