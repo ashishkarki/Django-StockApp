18b6b71b814e6a8aaa25ed9c1267f3688ae62809
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2021-02-07 16:15:02 +0545

    Implementation for add ticker to portfolio

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import StockForm
 
 # Create your views here.
 
@@ -7,12 +8,12 @@ from django.shortcuts import render
 def home(request):
     import requests
     import json
-    from quotes import my_secrets
+    from .my_secrets import IEX_CLOUD_API_KEY
 
     if request.method == 'POST':
         ticker = request.POST['ticker']
         api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token"
-                                                                                        "=" + my_secrets.IEX_CLOUD_API_KEY)
+                                                                                        "=" + IEX_CLOUD_API_KEY)
         try:
             api_response = json.loads(api_request.content)
         except Exception:
@@ -32,4 +33,19 @@ def about(request):
 
 
 def add_stock(request):
-    return render(request, 'add_stock.html', {})
+    from .models import Stock
+
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Stock addition successful")
+
+            return redirect('add_stock')
+    else:
+        all_tickers = Stock.objects.all()
+
+        return render(request, 'add_stock.html', {
+            'all_tickers': all_tickers
+        })
