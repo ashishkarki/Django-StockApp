5adcb99a8113eb926d3e83646826adba64ddfee3
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2021-02-07 10:52:04 +0545

    adding features to allow users to enter a ticker symbol and get their stock quotes in basic form.

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -9,17 +9,22 @@ def home(request):
     import json
     from quotes import my_secrets
 
-    ticker = 'aapl'  # request.POST['ticker']
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token"
-                                                                                    "=" + my_secrets.IEX_CLOUD_API_KEY)
-    try:
-        api_response = json.loads(api_request.content)
-    except Exception as e:
-        api_response = "Error"
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token"
+                                                                                        "=" + my_secrets.IEX_CLOUD_API_KEY)
+        try:
+            api_response = json.loads(api_request.content)
+        except Exception:
+            api_response = "Error"
 
-    return render(request, 'home.html', {
-        'api_response': api_response
-    })
+        return render(request, 'home.html', {
+            'api_response': api_response
+        })
+    else:
+        return render(request, 'home.html', {
+            'info': 'Please enter a ticker symbol above..'
+        })
 
 
 def about(request):
